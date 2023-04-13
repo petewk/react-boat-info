eee182bb51d171f28cbda7d4c1eaf08460870ada
petewk <petewk1991@gmail.com>
2023-04-13 13:50:11 +0100

    deploying to Heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const fs = require('fs');
+const path = require('path');
 
 
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 
 
--- server/sqldb/database.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2';
-
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Holborough4691!',
-    database: 'boat-info'
-}).promise();
-

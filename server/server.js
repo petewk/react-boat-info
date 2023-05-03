49530e714516aa109bf5dcf572adb7cf1be2948d
petewk <petewk1991@gmail.com>
2023-05-03 14:15:41 +0100

    Revert "deploying to Heroku"
    This reverts commit eee182bb51d171f28cbda7d4c1eaf08460870ada.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const fs = require('fs');
-const path = require('path');
 
 
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 
 
